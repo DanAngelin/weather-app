f8991cc458cff6f26405371555cbf0b3f4ea5116
Dan Angelin <dan.angelin@hotmail.com>
2021-12-30 17:37:15 +0200

    added sunset - sunrise, some icons, css style

--- js/app.js
+++ js/app.js
@@ -1,50 +1,60 @@
-const cityName = document.querySelector('.city-name');
-const temperature = document.querySelector('.temp');
-const temperatureMin = document.querySelector('.temp-min');
-const temperatureMax = document.querySelector('.temp-max');
-const feelsLike = document.querySelector('.feels-like');
-const time = document.querySelector('.time');
-const descriptionWeather = document.querySelector('.description-weather');
-const humidityApp = document.querySelector('.humidity-p');
-const pressureApp = document.querySelector('.pressure-p')
 
 
+// I use API from openweathermap.org
 
 const keyApi = 'e3e59ccb8a47bc1f943eaf0baa3ce684';
 
 const getWeatherApp = async querry => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=bucharest&appid=${keyApi}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=bucharest&units=metric&appid=${keyApi}`);
     const wheaterApp = await response.json();
     console.log(wheaterApp);
 
     try {
-        let {name: city, sys: {country}, main: {temp, temp_min, temp_max, feels_like, humidity, pressure},
-            weather: [{description}]} = wheaterApp;
-        console.log(city);
 
-        cityName.innerText = `Weather in ${city}, ${country}`;
-        temperature.innerHTML = `${(temp - 273.15).toFixed(1)}&deg;C`;
-        temperatureMin.innerHTML = `Min:${(temp_min - 273.15).toFixed(1)}&deg;C`;
-        temperatureMax.innerHTML = `Max:${(temp_max - 273.15).toFixed(1)}&deg;C`;
-        feelsLike.innerHTML = `Feels like ${(feels_like - 273.15).toFixed(1)}&deg;C`;
-        descriptionWeather.innerText = `${description.toUpperCase()}`;
-        humidityApp.innerText = `Humidity: ${humidity}%`;
-        pressureApp.innerText = `Pressure: ${pressure} hPa`;
+        // Data Destructuring
+        let {name: city, sys: {country, sunrise, sunset}, 
+            main: {temp, temp_min, temp_max, feels_like, humidity, pressure},
+            weather: [{main: description, icon}], wind: {speed},
+            dt, timezone} = wheaterApp;
+            console.log(timezone)
+
+        // City
+        document.querySelector('.city-name').innerText = `Weather in ${city}, ${country}`;
+
+        // Temp
+        document.querySelector('.temp').innerText = `${temp.toFixed(1)} °C`;
+
+        // Temp Min - Max
+        document.querySelector('.temp-min').innerText = `Min:${temp_min.toFixed(1)} °C`;
+        document.querySelector('.temp-max').innerText = `Max:${temp_max.toFixed(1)} °C`;
+
+        // Feels Like
+        document.querySelector('.feels-like').innerText = `Feels like ${feels_like.toFixed(1)} °C`;
+
+        // Weather Description - Humidity - Pressure - Wind Speed
+        document.querySelector('.description-weather').innerText = `${description}`;
+        document.querySelector('.humidity-p').innerText = `${humidity}%`;
+        document.querySelector('.pressure-p').innerText = `${pressure} hPa`;
+        document.querySelector('.wind-p').innerText = `${speed} m/s`;
+        document.querySelector('.icon').src = `http://openweathermap.org/img/wn/${icon}.png`;
+
+        // Timezone
+        // I use this method from 
+        //https://stackoverflow.com/questions/62690963/how-can-i-get-the-current-time-using-timezone-offset-using-moment-js;
+        const timezoneInMinutes = timezone / 60;
+        const currentTime = moment().utcOffset(timezoneInMinutes).format("LLL");
+        document.querySelector('.time').innerText = `${currentTime}`;
+        console.log(currentTime)
+
+        // Sunrise - Sunset
+        const sunriseTime = moment.unix(sunrise).utcOffset(timezoneInMinutes).format('LT');
+        const sunsetTime = moment.unix(sunset).utcOffset(timezoneInMinutes).format('LT');
+        document.querySelector('.sunrise p').innerText = `${sunriseTime}`;
+        document.querySelector('.sunset p').innerText = `${sunsetTime}`;
+
     } catch (error) {
         console.log(error);
     }
 }
 
 getWeatherApp();
-
-const getTime = new Date();
-let month = getTime.getMonth();
-let monthsArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-month = monthsArray[month];
-let hour = getTime.getHours();
-let minute = getTime.getMinutes();
-hour = (hour < 10 ? "0" : "") + hour;
-minute = (minute < 10 ? "0" : "") + minute;
-console.log(month)
-// const year = getTime.getFullYear
-time.innerText = `${getTime.getDate()} ${month} ${getTime.getFullYear()}, ${hour}:${minute}`;
