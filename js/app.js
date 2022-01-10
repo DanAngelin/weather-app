85eb8f1df806c8bf747ff5485b36312322ac3ae4
Dan Angelin <dan.angelin@hotmail.com>
2022-01-10 22:13:12 +0200

    add media queries & minor changes

--- js/app.js
+++ js/app.js
@@ -7,13 +7,12 @@ const getWeatherApp = async (weathercity) => {
     `https://api.openweathermap.org/data/2.5/weather?q=${weathercity}&units=metric&appid=${keyApi}`
   );
   if (response.status == 404) {
-    document.querySelector(".not-found").style.display = "block";
+    document.querySelector(".not-found").textContent = "No results found 😞!";
   } else {
-    document.querySelector(".not-found").style.display = "none";
+    document.querySelector(".not-found").textContent = "";
   }
 
   const wheaterApp = await response.json();
-  console.log(wheaterApp, weathercity);
 
   try {
     // Data Destructuring
@@ -30,18 +29,18 @@ const getWeatherApp = async (weathercity) => {
 
     // City
 
-    document.querySelector(".city-name").innerText = `Weather in ${city}, ${country}`;
+    document.querySelector(".city-name").textContent = `Weather in ${city}, ${country}`;
 
     // Temp
 
-    document.querySelector(".temp").innerText = `${temp.toFixed(1)} °C`;
+    document.querySelector(".temp").textContent = `${temp.toFixed(1)} °C`;
 
     // Temp Min - Max
 
-    document.querySelector(".temp-min").innerText = `Min:${temp_min.toFixed(
+    document.querySelector(".temp-min").textContent = `Min:${temp_min.toFixed(
       1
     )} °C`;
-    document.querySelector(".temp-max").innerText = `Max:${temp_max.toFixed(
+    document.querySelector(".temp-max").textContent = `Max:${temp_max.toFixed(
       1
     )} °C`;
 
@@ -52,20 +51,18 @@ const getWeatherApp = async (weathercity) => {
     ).innerText = `Feels like ${feels_like.toFixed(1)} °C`;
 
     // Weather Description - Humidity - Pressure - Wind Speed
-    document.querySelector(".description-weather").innerText = `${description}`;
-    document.querySelector(".humidity-p").innerText = `${humidity}%`;
-    document.querySelector(".pressure-p").innerText = `${pressure} hPa`;
-    document.querySelector(".wind-p").innerText = `${speed} m/s`;
-    document.querySelector(
-      ".icon"
-    ).src = `http://openweathermap.org/img/wn/${icon}.png`;
+    document.querySelector(".description-weather").textContent = `${description}`;
+    document.querySelector(".humidity-p").textContent = `${humidity}%`;
+    document.querySelector(".pressure-p").textContent = `${pressure} hPa`;
+    document.querySelector(".wind-p").textContent = `${speed} m/s`;
+    document.querySelector(".icon").src = `http://openweathermap.org/img/wn/${icon}.png`;
 
     // Timezone
     // I use this method from
     //https://stackoverflow.com/questions/62690963/how-can-i-get-the-current-time-using-timezone-offset-using-moment-js;
     const timezoneInMinutes = timezone / 60;
     const currentTime = moment().utcOffset(timezoneInMinutes).format("LLL");
-    document.querySelector(".time").innerText = `${currentTime}`;
+    document.querySelector(".time").textContent = `${currentTime}`;
 
     // Sunrise - Sunset
     const sunriseTime = moment
@@ -76,8 +73,8 @@ const getWeatherApp = async (weathercity) => {
       .unix(sunset)
       .utcOffset(timezoneInMinutes)
       .format("LT");
-    document.querySelector(".sunrise p").innerText = `${sunriseTime}`;
-    document.querySelector(".sunset p").innerText = `${sunsetTime}`;
+    document.querySelector(".sunrise p").textContent = `${sunriseTime}`;
+    document.querySelector(".sunset p").textContent = `${sunsetTime}`;
 
     // Background Change by City or Country
 
@@ -98,7 +95,6 @@ const getValueSrc = () => {
 };
 
 btnSearch.addEventListener("click", () => getWeatherApp(getValueSrc()));
-console.log(getWeatherApp(getValueSrc()));
 
 // Enter key event input
 
@@ -111,4 +107,4 @@ inputEnter.addEventListener("keyup", (event) => {
 });
 
 let date = new Date();
-document.querySelector('.year').innerText = date.getFullYear()
+document.querySelector('.year').textContent = date.getFullYear()
